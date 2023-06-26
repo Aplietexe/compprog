d8a59cad1c35dd8d43be832210fe9c1f39d9f22d
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2023-06-26 02:10:32 -0300

    Update sparse_table.cpp

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,11 +1,16 @@
+const ll K = 63 - __builtin_clzll(MAXN) + 1;
+
 #define oper min
 vvll st(K, vll(1 << K));
 void st_init(vll &a)
 {
-    fori(i, 0, n) st[0][i] = a[i];
+    ll n = a.size();
+    fori(i, 0, n)
+        st[0][i] = a[i];
 
-    fori(k, 1, K) fori(i, 0, n - (1 << k) + 1)
-        st[k][i] = oper(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
+    fori(k, 1, K)
+        fori(i, 0, n - (1 << k) + 1)
+            st[k][i] = oper(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
 }
 
 ll st_query(int s, int e) // [s, e)
