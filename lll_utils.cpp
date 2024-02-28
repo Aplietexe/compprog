516bd044ebc404551c176ee1cfe067851486b553
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2024-02-28 01:19:21 -0300

    Create lll_utils.cpp

---
+++ lll_utils.cpp
@@ -0,0 +1,21 @@
+constexpr lll abs(lll& v) { return v < 0 ? -v : v; }
+istream& operator>>(istream& is, lll& value) {
+    ll x;
+    is >> x;
+    value = x;
+    return is;
+}
+ostream& operator<<(ostream& os, lll value) {
+    if (value < 0) {
+        os << '-';
+        value = -value;
+    }
+    if (numeric_limits<unsigned long long>::max() >= value) return os << (unsigned long long)value;
+    string s;
+    do {
+        s += value % 10 + '0';
+        value /= 10;
+    } while (value);
+    rof(i, SZ(s), 0) os << s[i];
+    return os;
+}
